2814348964dec2b7797cb3e7f16f5032e724cac4
Morgan Stoodley <morgan@teamstoodley.com>
2025-11-20 16:15:09 +1100

    Added 3d

--- 6plus.py
+++ 6plus.py
@@ -1,5 +1,7 @@
+import math
 import random
 import itertools
+from matplotlib import cm
 import matplotlib.pyplot as plt
 from collections import Counter
 
@@ -18,7 +20,7 @@ hand_category = {
 
 HAND_SIZE = 2
 COMMUNITY_SIZE = 5
-players = 6
+players = 2
 
 suits = ["S", "C", "D", "H"]
 values = ["6", "7", "8", "9", "T", "J", "Q", "K", "A"]
@@ -167,18 +169,18 @@ def simulate(n_sims):
 
     return win_counts, dealt_counts
 
-win_counts, dealt_counts = simulate(100000)
+# win_counts, dealt_counts = simulate(200000)
 
-strength = {
-    label: win_counts[label] / dealt_counts[label]
-    for label in dealt_counts
-}
+# strength = {
+#     label: win_counts[label] / dealt_counts[label]
+#     for label in dealt_counts
+# }
 
-# # Top 10
-# relevant = sorted(strength.items(), key=lambda x: x[1], reverse=True)[:10]
+# # # Top 10
+# # relevant = sorted(strength.items(), key=lambda x: x[1], reverse=True)[:10]
 
 # # Bottom 10
-# # relevant = sorted(strength.items(), key=lambda x: x[1], reverse=False)[:10]
+# relevant = sorted(strength.items(), key=lambda x: x[1], reverse=False)[:10]
 
 # labels = [label for label, _ in relevant]
 # vals   = [val for _, val in relevant]
@@ -203,31 +205,87 @@ strength = {
 
 #     return category_counts
 
-def simulate_all_hand_categories(n_sims):
-    category_counts = Counter()
+# def simulate_all_hand_categories(n_sims):
+#     category_counts = Counter()
 
-    for _ in range(n_sims):
-        hands, community = deal()
+#     for _ in range(n_sims):
+#         hands, community = deal()
 
-        for h in hands:
-            best_rank, _ = best5of7(h + community)
-            category = best_rank[0]
+#         for h in hands:
+#             best_rank, _ = best5of7(h + community)
+#             category = best_rank[0]
 
-            category_counts[hand_category[category]] += 1
+#             category_counts[hand_category[category]] += 1
 
-    return category_counts
+#     return category_counts
 
-sims = 200000
-category_counts = simulate_all_hand_categories(sims)
+# from mpl_toolkits.mplot3d import Axes3D
+# import numpy as np
+# import matplotlib.pyplot as plt
 
-labels = list(hand_category.values()) 
-vals   = [category_counts[l] / sims / 6 for l in labels]
+# sims = 10_00
 
-plt.figure()
-plt.bar(labels, vals)
-plt.xticks(rotation=45, ha="right")
-plt.title("Frequency of All Hand Types")
-plt.ylabel("Proportion")
-plt.tight_layout()
-plt.show()
+# player_values = list(range(2, 11))
+
+# order = [
+#     "Straight",
+#     "Full House",
+#     "Two Pair",
+#     "Three of a Kind",
+#     "Flush",
+#     "Pair",
+#     "Four of a Kind",
+#     "Straight Flush",
+#     "High Card"
+# ]
+
+# labels = order
+
+# num_players = len(player_values)
+# num_cats = len(labels)
+
+# data = np.zeros((num_players, num_cats), dtype=float)
+
+# for i, players in enumerate(player_values):
+#     category_counts = simulate_hand_categories(sims)
+
+#     for j, label in enumerate(labels):
+#         # data[i, j] = 0 if category_counts[label] == 0 else -math.log(1 - category_counts[label] / sims)
+#         data[i, j] = category_counts[label] / sims
+#     print(players)
+
+# fig = plt.figure()
+# ax = fig.add_subplot(111, projection="3d")
+
+# x_pos = np.arange(num_cats)
+# y_pos = np.arange(num_players)
+
+# xpos, ypos = np.meshgrid(x_pos, y_pos)
+# xpos = xpos.ravel()
+# ypos = ypos.ravel()
+# zpos = np.zeros_like(xpos)
+
+# dx = 0.5 * np.ones_like(xpos)
+# dy = 0.5 * np.ones_like(ypos)
+# dz = data.ravel()
+
+# norm = plt.Normalize(dz.min(), dz.max())  
+# colors = cm.viridis(norm(dz))
+
+# ax.bar3d(xpos, ypos, zpos, dx, dy, dz, color=colors, shade=True, zsort='average')
+
+# ax.set_title("Winning Hand Categories")
+# # ax.set_xlabel("Hand category")
+# ax.set_ylabel("Number of players")
+# ax.set_zlabel("Proportion")
+
+# ax.set_xticks(np.arange(num_cats) + 0.25)
+# ax.set_xticklabels(labels, rotation=45, ha="right")
+
+# ax.set_yticks(np.arange(num_players) + 0.25)
+# ax.set_yticklabels(player_values)
+
+# plt.tight_layout()
+# plt.show()
+
 
